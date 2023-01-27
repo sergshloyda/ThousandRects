3952407a75ce116969d67b8181b3782bad0d4e1a
sergshloyda <shloyders@gmail.com>
2023-01-27 15:52:42 +0300

    Add BScan in DeviceSettings

--- ThousandRectsInConcurentThreads/renderthread.h
+++ ThousandRectsInConcurentThreads/renderthread.h
@@ -38,18 +38,10 @@ private:
 	void MarkOutBackground(const DeviceSettings* pDeviceSettings);
 	void PlotBackground(QPainter& painter,const QRect& rect,const DeviceSettings* pDeviceSettings);
 	void DrawLabelChannel(QPainter&painter,const quint8 num_chan,const DeviceSettings* pDeviceSettings);
-	void PlotMnemo(QPainter& painter,const std::vector<ElementInfo*>& vec,const QRectF& rect);
-	void PlotCoord(QPainter& painter,const std::vector<ElementInfo*>& vec,const QRectF& rect);
-	void PlotThickRow(QPainter& painter,const std::vector<ElementInfo*>& vec,const QRectF& rect,const quint8 num_chan);
-	void PlotLaminationThickRow(QPainter& painter,const std::vector<ElementInfo*>& vec,const QRectF& rect,const quint8 num_chan);
-	void PlotDefectRow(QPainter& painter,const std::vector<ElementInfo*>& vec,const QRectF& rect,const quint8 num_chan);
 	void PlotBScanRow(QPainter& painter,const std::vector<ElementInfo*>& vec,const QRectF& rect,const quint8 num_chan);
-
-	void PlotErrSemiTransparent(QPainter & painter,	const quint8 &defect_flag,const float curr_x,const float next_x,const int curr_y_b,const int curr_height);
-	//void SetColors();
+	
 private:
 
-	//QImage _image;
 	int _visible_elements;
 	int _width;
 	int _height;
@@ -61,29 +53,9 @@ private:
 	QRectF _mnemo_plotter_rect;
 	std::vector<QRectF> _chan_label_rect_arr;
 	std::vector<QRectF> _chan_plotter_rect_arr; 
-	//const DeviceSettings* _pDeviceSettings;
-
+	
 	const int _label_txt_margin;
 
-	static const int plot_line_W=1;
-	static const quint8 MaxAmpl=255;
-
-	QColor _mnemo_background_color;
-	QColor _error_color;
-	QColor _thick_color;
-	QColor _sufficient_condition_color;
-	QColor _sep_line_color;
-	QColor _flaw_poper_color,_flaw_prod_color;
-	QColor _osc_background_color;
-
-	QColor _sub_strobs_color[NUM_STRBS][NUM_EXTRA_POR];
-	
-	QColor _ak_fill_color;
-	QColor _error_flag_incorrect_color;
-	QColor _error_flag_data_skip_color;
-	QColor _error_flag_no_ak_color;
-	QColor _error_flag_lamination_color;
-	QMutex* _pMutex;
 	DrawStrategyContainer* _pDrawStrategyContainer;
 
 
